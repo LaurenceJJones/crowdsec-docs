ce1dc0adf10e328ba1f89c8aea0db1e4a4b68dc8
Laurence <laurence.jones@live.co.uk>
2024-01-29 10:06:48 +0000

    wippy wip wip wip

--- crowdsec-docs/src/pages/index.js
+++ crowdsec-docs/src/pages/index.js
@@ -15,9 +15,14 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
+            className={`button button--secondary ${styles["button-xl"]}`}
             to="/products">
             Get Started Now 🚀
+          </Link>
+          <Link
+            className={`button button--secondary ${styles["button-xl"]}`}
+            to="/docs/next/intro">
+            Documentation 📖
           </Link>
         </div>
       </div>
--- crowdsec-docs/src/pages/products.js
+++ crowdsec-docs/src/pages/products.js
@@ -1,42 +1,44 @@
-import React from "react";
-import clsx from "clsx";
+import React, {useState} from "react";;
 import Layout from "@theme/Layout";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "./index.module.css";
 
 function HomepageHeader() {
+    const [securityEngine, setSecurityEngine] = useState(false);
     return (
-        <div className="container">
-            <h2>
-                Our Products
-            </h2>
+        <div className={`${styles["product-helper-container"]}`}>
+            <h1 className={`hero`}>
+                Which solution is right for you?
+            </h1>
             <div className={styles.buttons}>
                 <Link
-                    className={`button button--secondary button--lg ${styles.buttonxl}`}
+                    className={`button button--secondary button--lg ${styles["button-xl"]}`}
                     to="/products"
+                    onClick={() => setSecurityEngine(!securityEngine)}
                 >
                     Security Engine
                 </Link>
                 <Link
-                    className={`button button--secondary button--lg ${styles.buttonxl}`}
+                    className={`button button--secondary button--lg ${styles["button-xl"]}`}
                     to="/products"
                 >
                     Blocklists
                 </Link>
                 <Link
-                    className={`button button--secondary button--lg ${styles.buttonxl}`}
+                    className={`button button--secondary button--lg ${styles["button-xl"]}`}
                     to="/products"
                 >
                     CTI
                 </Link>
             </div>
+            {securityEngine && <>SE's</>}
             <div className={styles.buttons}>
                 <Link
-                    className={`button button--secondary button--lg ${styles.buttonxl}`}
+                    className={`button button--primary button--lg ${styles["button-xl"]} ${styles["help-button"]}`}
                     to="/products"
                 >
-                    Help
+                    Help me choose ⛑️
                 </Link>
             </div>
         </div>
